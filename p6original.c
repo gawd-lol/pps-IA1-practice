4facc39b4bceea9e42153a1b6579cf7cc16f29e6
gawd-lol <94883170+gawd-lol@users.noreply.github.com>
2022-02-02 10:10:55 +0000

    Finished

--- p6original.c
+++ p6original.c
@@ -6,16 +6,22 @@ void input_two_string(char a, char b)
   scanf("%s%s", a, b);
 }
 
-int strcmp(char a, char b)
+int strcmp(char *a, char *b)
 {
-  for(int i=0, a[i]!='\0' && b[i]!='\0'; i++)
+  for (int i=0; a[i]!='\0' && b[i]!='\0'; i++)
   {
     if(a[i]>b[i])
+    {
       return 1;
+    }
     if(a[i]<b[i])
+    {
       return -1;
+    }
     if(a[i]==b[i])
+    {
       continue;
+    }
   }
 }
 
